ddb2c0a4c8c963c447f78563c10e5dc88de47395
Florian Dupeyron <florian.dupeyron@mugcat.fr>
2023-06-28 11:57:06 +0200

    Test publish on button click

--- state/mqtt_connection.py
+++ state/mqtt_connection.py
@@ -7,13 +7,25 @@ MQTT client stuff
 import paho.mqtt.client as mqtt
 import logging
 
-conn            = None                            # Global object representing client connection
-process_message = lambda topic,usrdata,data: None # Process message callback
+conn            = None   # Global object representing client connection
+routes          = dict() # Topic/callback
 
 log = logging.getLogger("mqtt_connection")
 
-def __on_message(*args):
-    process_message(*args)
+def __on_message(client, userdata, message):
+    topic = message.topic
+    data  = message.payload
+
+    # Check if route exist
+    log.debug(f"Received {topic}: {data}")
+
+    clbk = routes.get(topic, None)
+
+    if clbk is not None:
+        clbk(data)
+    else:
+        log.warn(f"Received message on topic {topic}, but no associated callback, discarding")
+
 
 def init(broker_config):
     global conn
@@ -24,21 +36,41 @@ def init(broker_config):
 
     log.info(f"Init MQTT broker connection: host = {host}, port = {port}, client_id = {client_id or '<none>'}")
 
-    conn      = mqtt.Client(client_id)
-
-    conn.connect(host=host, port=port)
+    conn            = mqtt.Client(client_id)
     conn.on_message = __on_message
 
-def set_message_callback(x):
-    global process_message
-    process_message = x
+    conn.connect(host=host, port=port)
 
 def start():
     global conn
     log.debug("Start MQTT loop")
+
+    log.debug("-> Subscribe to registered topics")
+    for topic in routes.keys():
+        log.debug(f"  : {topic}")
+        conn.subscribe(topic, qos=0) # Keep qos=0 by default
+
     conn.loop_start()
 
 def stop():
     global conn
     log.debug("Stop MQTT loop")
     conn.loop_stop()
+
+def subscribe(topic, callback, qos=0):
+    log.debug(f"Subscribe to topic {topic}")
+
+    if topic in routes:
+        raise RuntimeError(f"Already subscribed to topic {topic}")
+
+    routes[topic] = callback
+
+    if conn is not None:
+        conn.subscribe(topic, qos)
+
+def publish(topic, payload=None, qos=0, retain=False):
+    log.debug(f"Publish to {topic}: {payload}, with qos={qos} and retain={retain}")
+    if conn is not None:
+        conn.publish(topic=topic, payload=payload, qos=qos, retain=retain)
+    else:
+        log.warning(f"Cannot publish on topic {topic}, as MQTT client is not init.")
--- tabs/control.py
+++ tabs/control.py
@@ -1,6 +1,10 @@
 from nicegui import ui
 import logging
 
+import time
+
+from state import mqtt_connection
+
 log = logging.getLogger("Control tab")
 
 def label():
@@ -10,4 +14,14 @@ def icon():
     return "build"
 
 def content():
-    ui.button("Click me!", on_click=lambda: log.info("This is info message!"))
+    button = ui.button("Click me!")
+    def button_onclick():
+        mqtt_connection.publish("ui/test_button/click", payload=f"{time.time()}".encode("ascii"))
+    button.on("click", button_onclick)
+
+    test_label = ui.label("")
+    def value_clbk(x):
+        log.info("Received some stuff!")
+        test_label.set_text(x.decode("ascii"))
+
+    mqtt_connection.subscribe("test_topic", callback = value_clbk)
